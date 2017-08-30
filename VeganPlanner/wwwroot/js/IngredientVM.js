413f292b6a3d0d8dfa8dbe476dde51ddaeea35b2
Judyv <Jvolta@vtechnologies.com>
2017-08-30 12:24:25 -0400

    try to separate ItemVM, RecipeVM, etc.

---
+++ VeganPlanner/wwwroot/js/IngredientVM.js
@@ -0,0 +1,26 @@
+﻿
+define(["js/ItemVM"], function (ItemVM) {
+
+    function IngredientVM() {
+        var self = this;
+
+        self.IngredientID = ko.observable();
+        self.Item = ko.observable();
+        self.ItemID = ko.observable();
+        self.Quantity = ko.observable();
+        self.RecipeID = ko.observable();
+        self.Units = ko.observable();
+
+        self.load = function (data) {
+            self.IngredientID(data.ingredientID);
+            var im = new ItemVM();
+            im.load(data.item);
+            self.Item(im)
+            self.ItemID(data.itemID);
+            self.Quantity(data.quantity);
+            self.RecipeID(data.recipeID);
+            self.Units(data.units);
+        }
+    }
+    return IngredientVM;
+});
---
+++ VeganPlanner/wwwroot/js/ItemVM.js
@@ -0,0 +1,48 @@
+﻿define(["js/RecipeVM"], function (RecipeVM) {
+
+    function ItemVM() {
+        var self = this;
+
+        self.ItemID = ko.observable();
+        self.Name = ko.observable().extend({ required: "Enter a Name" });
+        self.IsRecipe = ko.observable(false);
+        self.ServingSize = ko.observable();
+        self.ServingUnits = ko.observable();
+        self.Category = ko.observable();
+        self.UserID = ko.observable();
+        self.CaloriesPerServing = ko.observable();
+        self.ProteinPerServing = ko.observable();
+        self.IsPantryItem = ko.observable();
+        self.IsGF = ko.observable();
+        self.RecipeID = ko.observable();
+        self.Recipe = new RecipeVM();
+
+        self.CreatedByAdmin = ko.pureComputed(function () {
+            if (self.UserID() == "lvolta@umich.edu" || self.UserID() == "jvolta@vtechnologies.com") {
+                return true;
+            } else {
+                return false;
+            }
+        });
+
+        self.load = function (data) {
+            self.ItemID(data.itemID);
+            self.Name(data.name)
+            self.IsRecipe(data.isRecipe);
+            self.ServingSize(data.servingSize);
+            self.ServingUnits(data.servingUnits);
+            self.Category(data.category);
+            self.UserID(data.userID);
+            self.CaloriesPerServing(data.caloriesPerServing);
+            self.ProteinPerServing(data.proteinPerServing);
+            self.IsPantryItem(data.isPantryItem);
+            self.IsGF(data.isGF);
+            if (self.IsRecipe()) {
+                self.RecipeID(data.recipeID);
+                self.Recipe.load(data.recipe);
+            }
+        }
+    }
+
+    return ItemVM;
+});
